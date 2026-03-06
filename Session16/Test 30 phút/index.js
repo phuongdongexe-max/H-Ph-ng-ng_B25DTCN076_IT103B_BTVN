0614b96005b02219e54d41b7f7a8504398dd0f18
phuongdongexe-max <phuongdongexe@gmail.com>
2026-03-06 15:56:50 +0700

    Add files via upload

---
+++ Session16/Test 30 phút/index.js
@@ -0,0 +1,18 @@
+const render = () => {
+    const taskList = document.getElementById('taskInput');
+    taskList.a
+
+}
+    
+
+
+
+const add_task = () => {
+    const new_data = document.getElementById('taskIput');
+    const check_btn = document.getElementById('add-btn');
+    check_btn.addEventListener('click');
+    if (check_btn){
+        new_data.innerHTML;
+    }
+}
+
